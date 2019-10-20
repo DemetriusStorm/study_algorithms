c61c8432933179f63338102528bbd26d0b033f39
demetrius.storm@gmail.com <demetrius.storm@gmail.com>
2019-10-20 21:34:35 +0300

    Updated

--- binary_search.py
+++ binary_search.py
@@ -8,29 +8,28 @@ binary search
 
 
 def binary_search(list_in, item):
+    count_iter = 0
     low = 0                      # В переменных low и high хранятся границы
     high = len(list_in) - 1      # той части списка, в которой выполняется поиск
-    count = 0
     while low <= high:           # Пока эта часть не сократится до одного элемента..
-
         mid = (low + high) // 2  # ...проверяем средний элемент (отбрасываем дробную часть)
-        print('mid=', mid)
+        print('Середина списка за текущую итерацию; mid =', mid)
         guess = list_in[mid]
-        print('guess=', guess)
+        print('Нашли число? guess =', guess)
         if guess == item:        # Значение найдено
-            return mid, 'Всего итераций (проверок): ' + str(count)
+            return 'Нашли, у вашего числа {}, индекс {}. Всего итераций (проверок): {}'.format(item, mid, count_iter)
         elif guess > item:       # Много
             high = mid - 1
-            count += 1
+            count_iter += 1
         else:                    # Мало
             low = mid + 1
-            count += 1
+            count_iter += 1
         # print('Всего итерация:', count)
-    return None, 'Всего итераций (проверок): ' + str(count)  # Значение не существует
+    return 'Загаданное число отсутствует в списке. Всего итераций (проверок): {}'.format(count_iter)  # Значение не существует
 
 
 my_list = [1, 2, 3, 4, 5, 6, 7, 8]
-print(binary_search(my_list, -1))
+print(binary_search(my_list, 7))
 
 """
 Упраженение 1.1
